c5429b0141513724bf25b676345fe78e0752973d
João Guilherme Ponciano <115370264+joaogponciano@users.noreply.github.com>
2022-11-29 15:41:17 -0300

    Add Set Resolutions

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const catImage = document.querySelector(".show-img")
 const changeImage = document.querySelector(".submit")
 
+const resolution = document.querySelector(".resolution")
+
 const width = document.querySelector("#width-img")
 const height = document.querySelector("#height-img")
 
 changeImage.addEventListener('click', (e) => {
     catImage.style.backgroundImage = "url(" + `http://placekitten.com/${width.value}/${height.value}` + ")"
+})
+
+resolution.addEventListener('click', (e) => {
+    catImage.style.width = width.value + "px"
+    catImage.style.height = height.value + "px"
 })
 
 
