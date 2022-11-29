56526a203acdfa589f83d34e9a173ae961c9a186
João Guilherme Ponciano <115370264+joaogponciano@users.noreply.github.com>
2022-11-29 15:25:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const catImage = document.querySelector(".show-img")
 const changeImage = document.querySelector(".submit")
 
-const height = document.querySelector("#height-img")
 const width = document.querySelector("#width-img")
+const height = document.querySelector("#height-img")
 
 changeImage.addEventListener('click', (e) => {
-    catImage.style.backgroundImage = "url(" + `http://placekitten.com/${height.value}/${width.value}` + ")"
+    catImage.style.backgroundImage = "url(" + `http://placekitten.com/${width.value}/${height.value}` + ")"
 })
 
 
